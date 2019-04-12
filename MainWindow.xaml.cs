7f54596e2b3c90e5a771644bace73cba803c6a2f
田凡 <tianfan@nuctech.com>
2019-04-12 15:47:03 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using SomeServices;
+
+namespace GrpcToolsBuildIssue
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            new RequestMessage();
+        }
+    }
+}
